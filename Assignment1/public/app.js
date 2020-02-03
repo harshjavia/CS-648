077bc7ef5fdd14d15934d006f89894597625d402
Harsh <harsh.javia007@gmail.com>
2020-02-02 18:48:11 -0800

    file added

---
+++ Assignment1/public/app.js
@@ -0,0 +1,8 @@
+const element = React.createElement("div", null, React.createElement("h2", null, "Harsh Javia"), React.createElement("img", {
+  src: "./harsh.jpg",
+  alt: "Head Shot"
+}), React.createElement("p", null, "I am Harsh Javia and currently I am pursuing Masters in Computer Science at San Diego State University, and I am a avid enthusiast of swimming, table tennis and online games."), React.createElement("br", null), React.createElement("button", null, " ", React.createElement("a", {
+  href: "https://github.com/harshjavia",
+  target: "_blank"
+}, "Here is my GitHub Repository")));
+ReactDOM.render(element, document.getElementById('content'));
---
+++ Assignment1/src/app.jsx
@@ -0,0 +1,11 @@
+const element = (
+    <div>
+       <h2>Harsh Javia</h2>
+                <img src="./harsh.jpg" alt="Head Shot"></img>
+                <p>
+                    I am Harsh Javia and currently I am pursuing Masters in Computer Science at San Diego State University, and I am a avid enthusiast of swimming, table tennis and online games.
+                </p><br/>
+                <button> <a href="https://github.com/harshjavia" target="_blank">Here is my GitHub Repository</a></button>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('content'));
