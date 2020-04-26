9d173db7651e2a05b395c237fa7cbf19e72dc01a
Harsh <harsh.javia007@gmail.com>
2020-04-25 21:58:35 -0700

    assignment 6

---
+++ Assignment 6/ui/src/page.jsx
@@ -0,0 +1,42 @@
+/* eslint linebreak-style: ["error","windows"] */
+import React from 'react';
+import {
+  Nav, NavItem,
+  Navbar, Grid,
+  Glyphicon,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import Contents from './contents.jsx';
+
+function NavBar() {
+  return (
+    <Navbar>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Header>
+        <Navbar.Brand>Product Tracker</Navbar.Brand>
+      </Navbar.Header>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
+          <LinkContainer exact to="/">
+            <NavItem>Home</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/products">
+            <NavItem>Product List</NavItem>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default function Page() {
+  return (
+    <div>
+      <NavBar />
+      <Grid fluid>
+        <Contents />
+      </Grid>
+    </div>
+  );
+}
